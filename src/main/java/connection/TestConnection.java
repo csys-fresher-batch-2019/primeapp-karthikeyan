4d7e141bcad34ff24426de0fad7aaf00be514028
Karthikeyan P <bmpdk123@gmail.com>
2020-02-17 10:19:52 +0530

    Final

--- src/main/java/connection/TestConnection.java
+++ src/main/java/connection/TestConnection.java
@@ -3,8 +3,7 @@ package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import org.jdbi.v3.core.Jdbi;
-import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
 
 
 public class TestConnection {
@@ -16,17 +15,6 @@ public class TestConnection {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localHost:1521:XE","system","oracle");
 		return connection;
-	}
-	public static Jdbi getJdbi() {
-		Jdbi jdbi = null;
-		try {
-			Connection connection = getConnection();
-			jdbi = Jdbi.create(connection);
-			jdbi.installPlugin(new SqlObjectPlugin());
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		return jdbi;
 	}
 	
 
